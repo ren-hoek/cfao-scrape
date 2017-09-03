98f781571e0b0d67d30fd259ec49dc0661c75a9f
ren-hoek <ren-hoek@users.noreply.github.com>
2017-09-03 18:35:31 +0100

    fixed error in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 sudo apt-get install wget
 cd
 wget https://repo.continuum.io/archive/Anaconda2-4.4.0-Linux-x86_64.sh
-bash Anaconda3-4.4.0-Linux-x86_64.sh
-rm Anaconda3-4.4.0-Linux-x86_64.sh
+bash Anaconda2-4.4.0-Linux-x86_64.sh
+rm Anaconda2-4.4.0-Linux-x86_64.sh
 conda create --name cfao pip
 source activate cfao
 pip install -r requirements.txt
